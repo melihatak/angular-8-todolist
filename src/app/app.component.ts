ce87715dbd0726ec6e59f5eb36ab46fcc4fd07cd
ismetsezer <ismetsezer1996@gmail.com>
2019-08-11 00:35:14 +0300

    seperated icon rows

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { faSearch,faMapMarker,faLocationArrow,faUser,faThermometer,faAddressBook} from '@fortawesome/free-solid-svg-icons'
+import { temporaryAllocator } from '@angular/compiler/src/render3/view/util';
 
 
 @Component({
@@ -10,6 +11,26 @@ import { faSearch,faMapMarker,faLocationArrow,faUser,faThermometer,faAddressBook
 
 export class AppComponent {
   title = 'Example';
- icons=[faSearch,faMapMarker,faLocationArrow,faUser,faThermometer,faAddressBook]
-configuration ={line:3}
+  icons=[faSearch,
+          faMapMarker,
+          faLocationArrow,
+          faUser,
+          faThermometer,
+          faAddressBook
+        ]
+        
+  configuration ={line:2}
+  
+  seperateLines(arr){
+    let n = this.configuration.line ; 
+    var chunkLength = Math.max(arr.length/n ,1);
+     var chunks = [];
+     for (var i = 0; i < n; i++) {
+         if(chunkLength*(i+1)<=arr.length)chunks.push(arr.slice(chunkLength*i, chunkLength*(i+1)));
+     }
+     return chunks;
+  }
+ 
 }
+
+
